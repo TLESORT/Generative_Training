9e71f66e8377ffc0abe740d754324816c9bcecfd
TLESORT <t.lesort@gmail.com>
2017-10-23 16:29:05 +0200

    no shuffle

--- load_dataset.py
+++ load_dataset.py
@@ -15,17 +15,17 @@ def load_dataset(dataset, batch_size, num_examples=60000, defaut='tim'):
         data_loader = DataLoader(datasets.MNIST(path + 'mnist', train=True, download=True,
             transform=transforms.Compose(
                 [transforms.ToTensor()])),
-            batch_size=batch_size, shuffle=True, sampler=SubsetRandomSampler(range(num_examples)))
+            batch_size=batch_size, sampler=SubsetRandomSampler(range(num_examples)))
     elif dataset == 'fashion-mnist':
         if fas:
             data_loader = DataLoader(
                 datasets.FashionMNIST(path + 'fashion-mnist', train=True, download=True, transform=transforms.Compose(
                     [transforms.ToTensor()])),
-                batch_size=batch_size, shuffle=True, sampler=SubsetRandomSampler(range(num_examples)))
+                batch_size=batch_size, sampler=SubsetRandomSampler(range(num_examples)))
         else:
             data_loader = data.DataLoader(
                     fashion('fashion_data', train=True, download=True, transform=transforms.ToTensor()),
-                    batch_size=batch_size, shuffle=True,  sampler=SubsetRandomSampler(range(num_examples)), num_workers=1, pin_memory=True)
+                    batch_size=batch_size,  sampler=SubsetRandomSampler(range(num_examples)), num_workers=1, pin_memory=True)
     elif dataset == 'cifar10':
         if num_examples > 50000: num_examples=50000 # does not work if num_example > 50000
         transform = transforms.Compose(
@@ -34,10 +34,10 @@ def load_dataset(dataset, batch_size, num_examples=60000, defaut='tim'):
 
         trainset = datasets.CIFAR10(root=path+'cifar10', train=True,
                    download=True, transform=transform)
-        data_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, sampler=SubsetRandomSampler(range(num_examples)), num_workers=8)
+        data_loader = DataLoader(trainset, batch_size=batch_size, sampler=SubsetRandomSampler(range(num_examples)), num_workers=8)
     elif dataset == 'celebA':
         data_loader = utils.load_celebA(path + 'celebA', transform=transforms.Compose(
-            [transforms.CenterCrop(160), transforms.Scale(64), transforms.ToTensor()]), batch_size=batch_size, shuffle=True,
+            [transforms.CenterCrop(160), transforms.Scale(64), transforms.ToTensor()]), batch_size=batch_size,
             sampler=SubsetRandomSampler(range(num_examples)))
 
     return data_loader
