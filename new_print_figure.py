85471e63fa6b8730580cef5b0ad768b4867e96b2
TLESORT <t.lesort@gmail.com>
2017-10-26 10:52:41 +0200

    fix for my results sorry Florian

--- new_print_figure.py
+++ new_print_figure.py
@@ -13,7 +13,8 @@ def plot_tau_training(save_dir, dataset, model_name):
     tau = 0.125
 
     #save_dir=[]
-    liste=["100","500","1000","5000","10000","50000"]
+    liste=["100","500","1000","5000","10000","60000"]
+    liste_classif=["100","500","1000","5000","10000","50000"]
     liste2=[100,500,1000,5000,10000,500000]
 
     save_dir2=os.path.join(save_dir, dataset, model_name)
@@ -39,10 +40,11 @@ def plot_tau_training(save_dir, dataset, model_name):
         max_tau.append(all_values.max())
         #baseline_gaussien.append(all_values[0])
         # Load baseline
+        if j == '60000': j='50000'
         name2 = os.path.join(save_dir, dataset, 'Classifier', 'num_examples_' + j, 'best_score_classif_ref' + dataset + '.txt')
         baseline.append(np.array(np.loadtxt(name2)).max())
         # Load baseline gauss
-        name2 = os.path.join(save_dir, dataset, 'Classifier', 'num_examples_' + j, 'best_score_classif_sigma_0.15' + dataset + '.txt')
+        name2 = os.path.join(save_dir, dataset, 'Classifier', 'num_examples_' + j, 'best_score_classif_sigma_0.125' + dataset + '.txt')
         baseline_gaussien.append(np.array(np.loadtxt(name2)).max())
 
         print(all_values.shape)
@@ -58,14 +60,14 @@ def plot_tau_training(save_dir, dataset, model_name):
     print(baseline)
     print(val.shape)
     for i in range(8):
-	plt.plot(liste2, val[:,i], label=str(tau * (i+1)))
-    plt.plot(liste2, baseline, linewidth=2, label='Baseline')
-    plt.xlabel("Num Example")
-    plt.ylabel("Test accuracy")
-    plt.xscale('log')
-    plt.legend(loc=4, title='tau')
-    plt.title('Test accuracy for ' + model_name)
-    plt.savefig(os.path.join(save_dir2, dataset+'_'+model_name +'_tau_test_accuracy.png'))
+        plt.plot(liste2, val[:,i], label=str(tau * (i+1)))
+        plt.plot(liste2, baseline, linewidth=2, label='Baseline')
+        plt.xlabel("Num Example")
+        plt.ylabel("Test accuracy")
+        plt.xscale('log')
+        plt.legend(loc=4, title='tau')
+        plt.title('Test accuracy for ' + model_name)
+        plt.savefig(os.path.join(save_dir2, dataset+'_'+model_name +'_tau_test_accuracy.png'))
     #plt.clf()
     # plt.ylabel("test accuracy")
 
@@ -89,14 +91,16 @@ def plot_num_training(saveDir, dataset):
     tau = 0.125
 
     # save_dir=[]
-    liste_model = ["VAE", "CGAN"]  # ,"Classifier","WGAN","VAE","ACGAN"]
-    liste = ["100", "500", "1000", "5000", "10000", "50000"]
+    liste_model = ["VAE", "WGAN"]  # ,"Classifier","WGAN","VAE","ACGAN"]
+    liste = ["100", "500", "1000", "5000", "10000", "60000"]
+    liste_classif = ["100", "500", "1000", "5000", "10000", "50000"]
+    liste2=[100,500,1000,5000,10000,500000]
 
     for model in liste_model:
         save_dir = os.path.join(saveDir, dataset, model)
-	print(save_dir)
+        print(save_dir)
         all_values = []
-	max_tau = []
+        max_tau = []
         for j in liste:
             # save_dir.append(os.path.join(save_dir, dataset, model_name, 'num_examples_' + str(i)))
             print(j)
@@ -108,7 +112,7 @@ def plot_num_training(saveDir, dataset):
                 values.append(np.loadtxt(name_file))  # [train_loss, train_acc, test_loss, test_acc]
 
             values = np.array(values)
-	    max_tau.append(values.max())
+            max_tau.append(values.max())
 
             print(values.shape)
             #assert values.shape[0] == 8
@@ -132,18 +136,18 @@ def plot_num_training(saveDir, dataset):
         # assert max_value.shape[1] == 4
 
         # x = np.arange(0, 1.125, 0.125)
-	"""
-		for i in range(all_values.shape[1]):
-		    plt.plot(range(7), all_values[:, i], label=model + " Tau=" + str(i * tau), linestyle=next(style_c)) plt.legend()
-	"""
+    """
+	    for i in range(all_values.shape[1]):
+	        plt.plot(range(7), all_values[:, i], label=model + " Tau=" + str(i * tau), linestyle=next(style_c)) plt.legend()
+    """
     baseline = []
     baseline_gaussien = []
-    for j in liste:
-	    name2 = os.path.join(saveDir, dataset, 'Classifier', 'num_examples_' + j, 'best_score_classif_ref' + dataset + '.txt')
-	    baseline.append(np.array(np.loadtxt(name2)).max())
-	    # Load baseline gauss
-	    name2 = os.path.join(saveDir, dataset, 'Classifier', 'num_examples_' + j, 'best_score_classif_sigma_0.15' + dataset + '.txt')
-	    baseline_gaussien.append(np.array(np.loadtxt(name2)).max())
+    for j in liste_classif:
+        name2 = os.path.join(saveDir, dataset, 'Classifier', 'num_examples_' + j, 'best_score_classif_ref' + dataset + '.txt')
+        baseline.append(np.array(np.loadtxt(name2)).max())
+        # Load baseline gauss
+        name2 = os.path.join(saveDir, dataset, 'Classifier', 'num_examples_' + j, 'best_score_classif_sigma_0.125' + dataset + '.txt')
+        baseline_gaussien.append(np.array(np.loadtxt(name2)).max())
     plt.plot(liste2, baseline, label='Baseline')
     plt.plot(liste2, baseline_gaussien, label='Baseline + noise')
 
@@ -158,8 +162,14 @@ def plot_num_training(saveDir, dataset):
 
 # plot_tau_training('models','mnist','CVAE', 50)
 
-plot_tau_training('models','mnist','VAE')
-plt.clf()
-plot_tau_training('models','mnist','CGAN')
-plt.clf()
+#plot_tau_training('models','mnist','VAE')
+#plt.clf()
+#plot_tau_training('models','mnist','WGAN')
+#plt.clf()
+#plot_tau_training('models','fashion-mnist','VAE')
+#plt.clf()
+#plot_tau_training('models','fashion-mnist','WGAN')
+#plt.clf()
 plot_num_training('models', 'mnist')
+plt.clf()
+plot_num_training('models', 'fashion-mnist')
