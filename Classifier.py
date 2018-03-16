faadf0c749b38ef2bbcf4bdf19bfff0266e008f6
TLESORT <t.lesort@gmail.com>
2018-03-16 15:30:08 +0100

    add some code for FID to exeperiment later

--- Classifier.py
+++ Classifier.py
@@ -16,6 +16,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from Model_Classifiers import Cifar10_Classifier, CelebA_Classifier, LSUN_Classifier, Timagenet_Classifier, Fashion_Classifier, Mnist_Classifier
 from scipy.stats import entropy
 
+from scipy import linalg
+
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
@@ -386,14 +388,16 @@ class Trainer(object):
 
     def Inception_score(self):
 
+        eval_size = 500
+
         # 0. load reference classifier
 
         self.load(reference=True) # self.Classifier is now the reference classifier
 
         # 1. generate data
-        output_table = torch.Tensor(self.examples*self.batch_size, 10)
+        output_table = torch.Tensor(eval_size*self.batch_size, 10)
 
-        for i in self.num_examples:
+        for i in eval_size:
             data, target = self.generator.sample(self.batch_size)
             # 2. use the reference classifier to compute the output vector
             if self.gpu_mode:
@@ -435,6 +439,59 @@ class Trainer(object):
 
             kld += KLDiv(Q, torch.exp(P)).data.cpu()[0]
         print("Mean KLD : {} \n".format(kld / (len(self.test_loader.dataset))))
+
+    def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+        # stolen from https://github.com/bioinf-jku/TTUR/blob/master/fid.py
+
+        """Numpy implementation of the Frechet Distance.
+        The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+        and X_2 ~ N(mu_2, C_2) is
+                d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+
+        Stable version by Dougal J. Sutherland.
+        Params:
+        -- mu1 : Numpy array containing the activations of the pool_3 layer of the
+                 inception net ( like returned by the function 'get_predictions')
+                 for generated samples.
+        -- mu2   : The sample mean over activations of the pool_3 layer, precalcualted
+                   on an representive data set.
+        -- sigma1: The covariance matrix over activations of the pool_3 layer for
+                   generated samples.
+        -- sigma2: The covariance matrix over activations of the pool_3 layer,
+                   precalcualted on an representive data set.
+        Returns:
+        --   : The Frechet Distance.
+        """
+
+        mu1 = np.atleast_1d(mu1)
+        mu2 = np.atleast_1d(mu2)
+
+        sigma1 = np.atleast_2d(sigma1)
+        sigma2 = np.atleast_2d(sigma2)
+
+        assert mu1.shape == mu2.shape, "Training and test mean vectors have different lengths"
+        assert sigma1.shape == sigma2.shape, "Training and test covariances have different dimensions"
+
+        diff = mu1 - mu2
+
+        # product might be almost singular
+        covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+        if not np.isfinite(covmean).all():
+            msg = "fid calculation produces singular product; adding %s to diagonal of cov estimates" % eps
+            warnings.warn(msg)
+            offset = np.eye(sigma1.shape[0]) * eps
+            covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+        # numerical error might give slight imaginary component
+        if np.iscomplexobj(covmean):
+            if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+                m = np.max(np.abs(covmean.imag))
+                raise ValueError("Imaginary component {}".format(m))
+            covmean = covmean.real
+
+        tr_covmean = np.trace(covmean)
+
+        return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
 
 
     # save a classifier or the best classifier
